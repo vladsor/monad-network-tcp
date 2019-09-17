5477bc0e0ec65a56ebfcaf97c984d6747aaed9b4
Vladimir Sorokin <v.d.sorokin@gmail.com>
2019-09-18 01:38:42 +0300

    Refactoring

--- default.nix
+++ default.nix
@@ -1,16 +1,3 @@
-{ mkDerivation, base, bytestring, cprng-aes, crypto-random, ether_0_5_1_0
-, exceptions, monad-network-class, mtl, network, newtype-generics
-, stdenv, vstls, transformers
-}:
-mkDerivation {
-  pname = "monad-network-instances";
-  version = "0.0.0.1";
-  src = ./.;
-  libraryHaskellDepends = [
-    base bytestring cprng-aes crypto-random ether_0_5_1_0 exceptions
-    monad-network-class mtl network newtype-generics vstls transformers
-  ];
-  homepage = "http://github.com/vladsor/monad-network-instances";
-  description = "Instances MonadConnection for Network & TLS";
-  license = stdenv.lib.licenses.bsd3;
-}
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.haskellPackages.callCabal2nix "monad-network-instances" ./. {}
